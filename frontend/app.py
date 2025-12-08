5d4edc5129e7815766d10ce894659fdab6a8f145
Erik Unevik <erikunevik@gmail.com>
2025-12-08 20:17:16 +0100

    frontend

--- frontend/app.py
+++ frontend/app.py
@@ -1,30 +1,47 @@
-      
 import streamlit as st
 import requests
-from pathlib import Path
 
-def layout():
 
-    st.markdown("# AIengineering chatbot")
-    st.markdown("Ask anything related to Data engineering")
-    text_input = st.text_input(label="Ask a questions")
+APIURL = "http://127.0.0.1:8000/chatbot/query"
 
-    if st.button("Send") and text_input != "":
-        response = requests.post(
-           "http://127.0.0.1:8000/chatbot/query", json={"prompt": text_input}
-        )
+st.set_page_config(page_title="AIengineering Chatbot", page_icon="💻")
+st.title("AIengineering Chatbot 💡")
 
-        data = response.json()
+# Saves chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = [] 
 
-        st.markdown("## Question:")
-        st.markdown(text_input)
+# Shows history
+for msg in st.session_state.messages:
+    with st.chat_message(msg["role"]):
+        st.markdown(msg["content"])
 
-        st.markdown("## Answer:")
-        st.markdown(data["answer"])
+# Chatwindow
+if prompt := st.chat_input("Ask anything related to Data engineering"):
+    # add message
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
 
-        st.markdown("## Source:")
-        st.markdown(data["filepath"])
- 
+    # Make api call
+        resp = requests.post(APIURL, json={"prompt": prompt})
+        resp.raise_for_status()
+        data = resp.json()  
 
-if __name__ == "__main__":
-    layout()
+        answer = data.get("answer")
+        filename = data.get("filename")
+       
+        # Bot answer
+        if filename:
+            bot_text = f"{answer}\n\n_**Source📖**: {filename}_"
+
+    # Add the bots answer to history
+    st.session_state.messages.append({"role": "assistant", "content": bot_text})
+    with st.chat_message("assistant"):
+        st.markdown(bot_text)
+
+# Deleteing chat
+if st.button("Rensa chatt"):
+    st.session_state.messages = []
+    st.rerun()
+
